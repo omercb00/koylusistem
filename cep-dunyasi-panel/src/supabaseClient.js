153ab3c41d75087ad08892959adef74f9146c3bc
omercb00 <omercan380088@gmail.com>
2026-05-15 16:30:46 +0300

    supabase sabit baglanti

--- cep-dunyasi-panel/src/supabaseClient.js
+++ cep-dunyasi-panel/src/supabaseClient.js
@@ -1,6 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://qlpegqfyktapmvcunaiq.supabase.co";
+
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFscGVncWZ5a3RhcG12Y3VuYWlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2ODA1MzEsImV4cCI6MjA5NDI1NjUzMX0.9RiYCTWhG-z0OoW_h1MCs4rKEu_yYzPCL0W87_rVuT0";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
